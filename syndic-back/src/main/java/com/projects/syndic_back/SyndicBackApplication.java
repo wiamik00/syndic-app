b3e14c64843379cfd922e138db2bf9c09e178905
Wiam IDRISSI KHAMLICHI <khamlichiwiam2000@gmail.com>
2025-11-06 15:35:05 +0100

    Project Structure & Dependency Setup

---
+++ syndic-back/src/main/java/com/projects/syndic_back/SyndicBackApplication.java
@@ -0,0 +1,13 @@
+package com.projects.syndic_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyndicBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyndicBackApplication.class, args);
+	}
+
+}
---
+++ syndic-back/src/test/java/com/projects/syndic_back/SyndicBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.syndic_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyndicBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
